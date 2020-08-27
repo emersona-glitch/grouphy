131f0ab53638db0147e38e573b82a586060f5dca
riley_ww77 <riley_ww77@yahoo.com>
2020-08-27 14:58:32 -0500

    commit

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -6,7 +6,7 @@ class Favorite extends Component {
         category: ''
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getFavorites()
     }
 
@@ -15,34 +15,53 @@ class Favorite extends Component {
     }
 
     handleCategory = (event) => {
-        
         console.log('setting category')
-        this.setState({
-            category: event.target.value
-        })
-
+        switch (event.target.value) {
+            case 'funny':
+                this.setState({
+                    category: 1
+                })
+            case 'cohort':
+                this.setState({
+                    category: 2
+                })
+            case 'cartoon':
+                this.setState({
+                    category: 3
+                })
+            case 'nsfw':
+                this.setState({
+                    category: 4
+                })
+            case 'meme':
+                this.setState({
+                    category: 5
+                })
+        }
     }
 
     handleClick = () => {
-        this.props.dispatch({ type: 'SET_CATEGORY', payload: this.state.category})
+        this.props.dispatch({ type: 'PUT_CATEGORY', payload: this.state.category })
     }
-    
+
     render() {
         return (
             <div>
                 {this.props.reduxState.favoritesReducer.map((favorite, i) => {
                     return (
-                        <ul key={i}>
-                            <img src= {favorite.image} alt='a favorite gif'></img>
-                        </ul>
-                        <select name="categories" onChange={this.handleCategory}>
-                            <option value='funny'>Funny</option>
-                            <option value='cohort'>Cohort</option>
-                            <option value='cartoon'>Cartoon</option>
-                            <option value='nsfw'>NSFW</option>
-                            <option value='meme'>Meme</option>
-                        </select>
-                        <button onClick={this.handleClick}>Add to Category</button>
+                        <>
+                            <ul key={i}>
+                                <img src={favorite.image} alt='a favorite gif'></img>
+                            </ul>
+                            <select name="categories" onChange={this.handleCategory}>
+                                <option value='funny'>Funny</option>
+                                <option value='cohort'>Cohort</option>
+                                <option value='cartoon'>Cartoon</option>
+                                <option value='nsfw'>NSFW</option>
+                                <option value='meme'>Meme</option>
+                            </select>
+                            <button onClick={this.handleClick}>Add to Category</button>
+                        </>
                     )
                 })}
             </div>
--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ const giphyListReducer = (state = [], action) => {
     return state;
 }
 
+const favoritesReducer = (state = [], action) => {
+    if (action.type === 'SET_FAVORITES'){
+        return action.payload;
+    } 
+    return state;
+}
+
 //get request path app.js -> index.js (saga) -> server.js -> database
 function* fetchSearch() {
     //try catch for errors
@@ -31,9 +38,31 @@ function* fetchSearch() {
     }
 }
 
+function* fetchFavorites(){
+    try{
+        let response = yield axios.get('/api/favorite');
+        console.log(response.data);
+        yield put ({type: 'SET_FAVORITES', payload: response.data});
+    } catch (error) {
+        console.log('error in getting favorites', error)
+    }
+}
+
+function* putCategory(){
+    try{
+        
+
+    } catch(error){
+        console.log('error in put category', error)
+    }
+}
+
+
 //saga root, watching for actions from app.js
 function* watcherSaga() {
     yield takeEvery('FETCH_SEARCH', fetchSearch);
+    yield takeEvery('FETCH_FAVORITES', fetchFavorites);
+    yield takeEvery('PUT_CATEGORY', putCategory);
 }
 
 //saga middleware creation
@@ -43,6 +72,7 @@ const store = createStore(
     //reducers run every time an action is placed
     combineReducers({
         giphyListReducer,
+        favoritesReducer
     }),
     //logger always goes last
     applyMiddleware(sagaMiddleware, logger),
