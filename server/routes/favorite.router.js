46869f448b74ca5dd54ee678d932188e9491d82b
riley_ww77 <riley_ww77@yahoo.com>
2020-08-28 11:11:31 -0500

    stashig

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -16,6 +16,20 @@ router.post('/', (req, res) => {
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
+  console.log('in put', req.params.id);
+  let queryText = `
+  UPDATE "favorites"
+  SET "category_id" = $1
+  WHERE "id" = $2;
+  `;
+  pool.query(queryText, [req.body, req.params.id])
+  .then ((result) => {
+    res.sendStatus(200)
+  }) .catch ((error) => {
+    console.log('error in PUT category', error);
+    res.sendStatus(500)
+  })
+
   res.sendStatus(200);
 });
 
